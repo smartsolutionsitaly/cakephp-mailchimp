ca9bcdc18030219f18e17bb6a6617ef10e7aad35
IT Development - Smart Solutions <dev@smartsolutions.it>
2018-07-26 14:33:12 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # cakephp-mailchimp
-MailChimp connector for CakePHP
+[![LICENSE](https://img.shields.io/github/license/smartsolutionsitaly/cakephp-mailchimp.svg)](LICENSE)
+[![packagist](https://img.shields.io/badge/packagist-smartsolutionsitaly%2Fcakephp--recaptcha-brightgreen.svg)](https://packagist.org/packages/smartsolutionsitaly/cakephp-mailchimp)
+[![issues](https://img.shields.io/github/issues/smartsolutionsitaly/cakephp-mailchimp.svg)](https://github.com/smartsolutionsitaly/cakephp-mailchimp/issues)
+[![CakePHP](https://img.shields.io/badge/CakePHP-3.5%2B-brightgreen.svg)](https://github.com/cakephp/cakephp)
+
+reCAPTCHA component for [CakePHP](https://github.com/cakephp/cakephp)
+
+## Installation
+
+You can install _cakephp-mailchimp_ into your project using [Composer](https://getcomposer.org).
+
+``` bash
+$ composer require smartsolutionsitaly/cakephp-mailchimp
+```
+
+## Configuration
+
+Put the MailChimp key and list in your application configuration file (usually config/app.php).
+
+``` php
+    'MailChimp' => [
+        'key' => 'API-KEY',
+        'lists' => [
+            'list1' => 'LIST-ID',
+            'list2' => 'LIST-ID'
+        ]
+    ];
+```
+
+## License
+Licensed under The MIT License
+For full copyright and license information, please see the [LICENSE](LICENSE)
+Redistributions of files must retain the above copyright notice.
+
+## Copyright
+Copyright (c) 2018 Smart Solutions S.r.l. (https://smartsolutions.it)
---
+++ src/Utility/MailChimp.php
@@ -0,0 +1,257 @@
+<?php
+/**
+ * cakephp-mailchimp (https://github.com/smartsolutionsitaly/cakephp-mailchimp)
+ * Copyright (c) 2018 Smart Solutions S.r.l. (https://smartsolutions.it)
+ *
+ * MailChimp connector for CakePHP
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @category  cakephp-plugin
+ * @package   cakephp-mailchimp
+ * @author    Lucio Benini <dev@smartsolutions.it>
+ * @copyright 2018 Smart Solutions S.r.l. (https://smartsolutions.it)
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * @link      https://smartsolutions.it Smart Solutions
+ * @since     1.0.0
+ */
+namespace SmartSolutionsItaly\CakePHP\MailChimp\Utility;
+
+use Cake\Core\Configure;
+use Cake\Http\Client;
+use Cake\Http\Client\Response;
+use Cake\Utility\Security;
+use Cake\Validation\Validation;
+use Exception;
+
+/**
+ * MailChimp connector.
+ *
+ * @author Lucio Benini <dev@smartsolutions.it>
+ * @since 1.0.0
+ */
+class MailChimp
+{
+
+    /**
+     * HTTP client.
+     *
+     * @var \Cake\Http\Client
+     * @since 1.0.0
+     */
+    protected $_client;
+
+    /**
+     * List ID.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected $_list;
+
+    /**
+     * Constructor.
+     *
+     * @since 1.0.0
+     */
+    public function __construct()
+    {
+        list ($key, $dc) = explode('-', Configure::read('MailChimp.key'));
+
+        $this->_client = new Client([
+            'host' => $dc . '.api.mailchimp.com/3.0/',
+            'scheme' => 'https',
+            'auth' => [
+                'username' => 'apikey',
+                'password' => $key
+            ],
+            'type' => 'json'
+        ]);
+    }
+
+    /**
+     * Sets the MailChimp's list from the configuration file.
+     *
+     * @param string $key
+     *            The key as setted in the configuration file.
+     * @return \SmartSolutionsItaly\CakePHP\MailChimp
+     * @since 1.0.0
+     */
+    public function setList(string $key)
+    {
+        return $this->setList(Configure::read('MailChimp.lists.' . $key));
+    }
+
+    /**
+     * Sets the MailChimp's list.
+     *
+     * @param string $name
+     *            The list name.
+     * @return \SmartSolutionsItaly\CakePHP\MailChimp
+     * @since 1.0.0
+     */
+    public function setList(string $name)
+    {
+        $this->_list = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the MailChimp's list.
+     *
+     * @return string The list name.
+     * @since 1.0.0
+     */
+    public function getList()
+    {
+        return $this->_list;
+    }
+
+    /**
+     * Gets the status of a subscription.
+     *
+     * @param string $email
+     *            The subscriber's e-mail.
+     * @return object|NULL Returns the MailChimp's response or NULL.
+     * @since 1.0.0
+     */
+    public function status(string $email)
+    {
+        if (Validation::email($email) && $this->getList()) {
+            try {
+                $res = $this->_client->get('lists/' . $this->getList() . '/members/' . Security::hash($email, 'md5'));
+            } catch (Exception $ex) {
+                return null;
+            }
+
+            return static::processResponse($res);
+        }
+
+        return null;
+    }
+
+    /**
+     * Unsubscribe the subscriber with the given e-mail from a list.
+     *
+     * @param string $email
+     *            The subscriber's e-mail.
+     * @param array $fields
+     *            The "merge" fields.
+     * @param unknown $language
+     *            The subscriber's language.
+     * @param unknown $ip
+     *            The subscriber's IP.
+     * @return object|NULL Returns the MailChimp's response or NULL.
+     * @since 1.0.0
+     */
+    public function subscribe(string $email, array $fields = [], $language = null, $ip = null)
+    {
+        if (Validation::email($email)) {
+            $vars = [
+                'email_address' => $email,
+                'status' => 'subscribed'
+            ];
+
+            if (! empty($fields)) {
+                $vars['merge_fields'] = $fields;
+            }
+
+            if (! $language) {
+                $language = Configure::read('App.defaultLocale');
+            }
+
+            if ($language) {
+                $language = explode('_', $language, 2);
+
+                if (! empty($language[0])) {
+                    $vars['language'] = $language[0];
+                }
+            }
+
+            if ($ip && Validation::ip($ip)) {
+                $vars['ip_signup'] = $ip;
+                $vars['ip_opt'] = $ip;
+            }
+
+            try {
+                $res = $this->_client->post('lists/' . $this->getList() . '/members/', json_encode($vars));
+            } catch (Exception $ex) {
+                return null;
+            }
+
+            return static::processResponse($res);
+        }
+
+        return null;
+    }
+
+    /**
+     * Unsubscribes the subscriber with the given e-mail from a list.
+     *
+     * @param string $email
+     *            The subscriber's e-mail.
+     * @return object|NULL Returns the MailChimp's response or NULL.
+     * @since 1.0.0
+     */
+    public function unsubscribe(string $email)
+    {
+        if (Validation::email($email)) {
+            try {
+                $res = $this->_client->patch('lists/' . $this->getList() . '/members/' . Security::hash($email, 'md5'), json_encode([
+                    'status' => 'unsubscribed'
+                ]));
+            } catch (Exception $ex) {
+                return null;
+            }
+
+            return static::processResponse($res);
+        }
+
+        return null;
+    }
+
+    /**
+     * Deletes the subscriber with the given e-mail from a list.
+     *
+     * @param string $email
+     *            The subscriber's e-mail.
+     * @return object|NULL Returns the MailChimp's response or NULL.
+     * @since 1.0.0
+     */
+    public function delete(string $email)
+    {
+        if (Validation::email($email)) {
+            try {
+                $res = $this->_client->delete('lists/' . $this->getList() . '/members/' . Security::hash($email, 'md5'));
+            } catch (Exception $ex) {
+                return null;
+            }
+
+            return static::processResponse($res);
+        }
+
+        return null;
+    }
+
+    /**
+     * Converts a response content to its object representation.
+     *
+     * @param \Cake\Http\Client\Response $res
+     *            The response object to process.
+     * @return object|NULL The response content to its object representation or NULL.
+     * @since 1.0.0
+     */
+    protected static function processResponse(Response $res)
+    {
+        $code = $res->getStatusCode();
+
+        if ($code >= 200 && $code < 300) {
+            return json_decode($res->getBody()->getContents());
+        } else {
+            return null;
+        }
+    }
+}
